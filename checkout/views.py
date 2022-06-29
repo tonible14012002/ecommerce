40a39da1441d9c88c32dc009370c3d99ceff572b
tonible14012002 <namanhble14012002@gmail.com>
2022-06-29 17:50:16 +0700

    fixing checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
+from http.client import OK
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from account.decorator import ajax_required
 from django.views.decorators.http import require_http_methods
 from cart.cart import Cart
@@ -9,7 +10,10 @@ from .form import CreateOrderForm
 def checkout(request):
     cart = Cart(request)
     context = {'cart': cart}
-    return render(request, 'checkout/checkout_page.html', context)
+    if request.method == 'POST':
+        return redirect('checkout:order_detail')
+    else:
+        return render(request, 'checkout/checkout_page.html', context)
 
 def order_detail(request):
     form = CreateOrderForm()
